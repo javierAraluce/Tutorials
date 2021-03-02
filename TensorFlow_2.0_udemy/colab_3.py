c3445ff7db68df98b80490cc800b1fa5f204ca55
kuasho <karaluce@gmail.com>
2021-03-02 13:07:46 +0100

    tensorflow tutorial

---
+++ TensorFlow_2.0_udemy/colab_3.py
@@ -0,0 +1,42 @@
+import numpy as np
+import datetime
+import tensorflow as tf
+from tensorflow.keras.datasets import fashion_mnist
+
+#Cargar el dataset Fashion Mnist 
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+X_train = X_train / 255.0
+X_train.shape
+X_test =  X_test / 255.0
+
+#Como cada imagen tiene 28x28 píxeles, usamos la función reshape en todo el dataset de entrenamiento para convertirlo 
+# en vectores de tamaño [-1 (todos los elementos), anchura * altura]
+X_train = X_train.reshape(-1, 28*28)
+X_test = X_test.reshape(-1, 28*28)
+
+#Redimensionamos el conjunto de testing del mismo modo
+X_test = X_test.reshape(-1, 28*28)
+X_test.shape
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Dense(units=128, activation='relu', input_shape=(784, )))
+model.add(tf.keras.layers.Dropout(0.2))
+
+#model.add(tf.keras.layers.Dense(units=64, activation='relu'))
+#model.add(tf.keras.layers.Dropout(0.2))
+#model.add(tf.keras.layers.Dense(units=32, activation='relu'))
+#model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.Dense(units=10, activation='softmax'))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
+model.summary()
+
+model.fit(X_train, y_train, epochs = 5, steps_per_epoch = 60000)
+test_loss, test_accuracy = model.evaluate(X_test, y_test, steps = 10000)
+print("Test accuracy: {}".format(test_accuracy))
+
+model_json = model.to_json()
+with open("fashion_model.json", "w") as json_file:
+    json_file.write(model_json)
+    
+model.save_weights("fashion_model.h5")
